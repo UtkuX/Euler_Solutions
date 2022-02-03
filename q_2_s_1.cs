3573ee113e2a60476270d386a7c9c5bd1dce9932
UtkuX <89424269+UtkuX@users.noreply.github.com>
2022-02-03 20:51:49 +0300

    Create q_2_s_1.cs

---
+++ q_2_s_1.cs
@@ -0,0 +1,22 @@
+using System.Threading.Channels;
+
+long first = 1;
+long second = 1;
+long no3 = 0;
+long ciftler = 0;
+bool rules = true;
+while (rules)
+{
+    no3 = first + second;
+    first = second;
+    second = no3;
+    if (second %2==0)
+    {
+        ciftler += second ;
+    }
+    if (no3 >= 4000000)
+    {
+        rules = false;
+    }
+}
+Console.WriteLine(ciftler);
