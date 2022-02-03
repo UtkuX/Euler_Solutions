2c56a835605f22ce0e141fa576aac9e260c97854
UtkuX <89424269+UtkuX@users.noreply.github.com>
2022-02-03 21:23:45 +0300

    Update q_1_s_1.cs

--- q_1_s_1.cs
+++ q_1_s_1.cs
@@ -9,11 +9,11 @@ namespace Question1
             int i;
             int topla = 0;
             for (i = 999; i > 0; i--)
-            {
+            { 
                 if (i%3 ==0 || i%5 ==0)
-                    {
-                       topla += i;
-                    }
+                {
+                    topla += i;
+                }
             }
             Console.WriteLine(topla);//233168
         }
